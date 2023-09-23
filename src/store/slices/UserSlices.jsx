d6b6eb97e763890577f7a947ea76216201226696
Asmita Porwal <asmita.porwal04@gmail.com>
2023-09-23 23:05:49 +0530

    first commit

---
+++ src/store/slices/UserSlices.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: [],
+    reducers: {
+        addUser(state, action) {
+            state.push(action.payload);
+            // console.log(action.payload);
+        },
+        removeUser(state, action) {
+            // console.log("hi" + action.payload);
+            state.splice(action.payload, 1);
+        },
+        deleteUsers(state, action) { },
+    },
+});
+
+console.log(userSlice.actions.addUser());
+
+export default userSlice.reducer;
+
+export const { addUser, removeUser } = userSlice.actions;
